6e2abeba0914eabc4473db8af234e0e7c8dc15b2
tentato <alepaw99@gmail.com>
2021-11-25 00:38:58 +0100

    border-almost-finished

--- Code/border.py
+++ Code/border.py
@@ -27,23 +27,36 @@ def find_top_pixel(contour, center):
     return top_pixel
 
 def find_avg_out(pixel, img):
+    img_copy = img.copy()
     sum = 0
-    i = 0
-    for i in range(60):
-        sum = sum + img[pixel[0], pixel[1]-30+i]
-        i=i+1
-    avg_out = sum/i
+    count = 0
+    for i in range(100):
+        try:    # in case not enough pixels out of the contour
+            sum += img_copy[pixel[1]-count, pixel[0]]
+            count += 1
+        except:
+            break
+    avg_out = sum / count
     return avg_out
 
-def find_avg_in(pixel, img):
-    avg_in = 0
+def find_avg_in(pixel, center, img):
+    img_copy = img.copy()
+    sum = 0
+    count = 0
+    range_pixels = (center[1] - pixel[1]) / 2
+
+     # checks all pixels between top pixel and center to get better average
+    for i in range(int(range_pixels)):
+        sum += img_copy[pixel[1]+count, pixel[0]]
+        count += 1
+    avg_in = sum / count
     return avg_in
 
 #############################
 #####	Main function	#####
 #############################
 
-def main_border(img, center, contours):
+def main_border(img, center, contours, log):
     B = 0.0
     angle = 45
     for i in range(7):
@@ -54,16 +67,27 @@ def main_border(img, center, contours):
         im_copy = cv2.warpAffine(im_copy, M, (w, h))
 
         contour_rotated = preprocessing.rotate_contour(contours, angle, center)
-        # cv2.drawContours(im_copy, [contour_rotated], 0, (0, 255, 250), 1)
-        preprocessing.show_image("Rotate {}".format(angle), im_copy)  
-        angle += 45
         top_pixel = find_top_pixel(contour_rotated, center)
         im_copy = preprocessing.convert_to_grayscale(im_copy)
         im_copy = preprocessing.gaussian_blur(im_copy)
+
+        # close to 0 - darker, close to 255 - lighter
         avg_out = find_avg_out(top_pixel, im_copy)
+        avg_in = find_avg_in(top_pixel, center, im_copy)
         print(avg_out)
+        print(avg_in)
+        cv2.drawContours(im_copy, [contour_rotated], 0, (0, 255, 250), 1)
+        
+        cv2.circle(im_copy, top_pixel, 3, (0, 255, 0), -1)
+        cv2.circle(im_copy, center, 3, (0, 255, 0), -1)
+        preprocessing.show_image("Rotate {}".format(angle), im_copy)  
+
         # plt.plot(im_copy[top_pixel[1]-30:top_pixel[1]+30, center[0]])
         # plt.show()
+
+        angle += 45
+    exit()
+    
     return B
 
     
--- Code/main.py
+++ Code/main.py
@@ -22,7 +22,7 @@ log_file = open("C:/Users/alepa/Desktop/Inz/Code/log_file.txt", "a")
 
 path = 'C:/Users/alepa/Desktop/Inz/Skin_melanoma/to_process/'
 
-full_path = sys.argv[1]
+# full_path = sys.argv[1]
 
 A = 0.0
 B = 0.0
@@ -33,25 +33,25 @@ C = 0.0
 #############################
 
 if __name__=="__main__":
-    # for file in os.listdir(path):
-    #     full_path = path + file
-    original_img, img_gray, gauss_img, img_thresh, img_closing, ROI_img, max_contour, rectangle_coordinates = preprocessing.main_preprocessing(full_path, log_file)
-    center = preprocessing.find_center(max_contour)
-    A = assymetry.main_assymetry(max_contour, center, log_file)
-    # B = border.main_border(original_img, center, max_contour)
-    C = color.main_color(original_img, max_contour, log_file)
+    for file in os.listdir(path):
+        full_path = path + file
+        original_img, img_gray, gauss_img, img_thresh, img_closing, ROI_img, max_contour, rectangle_coordinates = preprocessing.main_preprocessing(full_path, log_file)
+        center = preprocessing.find_center(max_contour)
+        A = assymetry.main_assymetry(max_contour, center, log_file)
+        B = border.main_border(original_img, center, max_contour, log_file)
+        # C = color.main_color(original_img, max_contour, log_file)
 
 
-    # ROI_img = cv2.circle(ROI_img, center, 3, (0, 255, 0), -1)
-    # preprocessing.show_image("Center", ROI_img)
+        # ROI_img = cv2.circle(ROI_img, center, 3, (0, 255, 0), -1)
+        # preprocessing.show_image("Center", ROI_img)
 
 
-    print("[Results] A=", A)
-    print("[Results] B=", B)
-    print("[Results] C=", C)
-    print("")
-    log_file.write("[Results] A={}\n".format(A))
-    log_file.write("[Results] B={}\n".format(B))
-    log_file.write("[Results] C={}\n".format(C))
+        print("[Results] A=", A)
+        print("[Results] B=", B)
+        print("[Results] C=", C)
+        print("")
+        log_file.write("[Results] A={}\n".format(A))
+        log_file.write("[Results] B={}\n".format(B))
+        log_file.write("[Results] C={}\n".format(C))
 
-    log_file.close()
+        log_file.close()
